14ec26e6fba1031e198ec5df61144049209cf6fa
Luigi <mr.unbekannt276@gmail.com>
2025-11-18 20:38:57 +0100

    first condition

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+    /*Testing*/
     // Player object Alex is created with a score of 0 and 0 affection (very sad)
     Player alex("Alex", 0, 0);
     Eun_Bi eun_bi("Eun-Bi", 30);
@@ -13,5 +14,30 @@ int main()
     std::cout << eun_bi.get_score() << std::endl;
     eun_bi.score_increase(2.7f);
     std::cout << eun_bi.get_score() << std::endl;
+
+    /*Actual Game Content*/
+    int input1;
+    
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Would you like to meet Eun-Bi (1) or Mal-Sook (2)?\n";
+    std::cin >> input1;
+    
+    if (input1 == 1){
+        std::cout << "Great, Eun-Bi will wear a nice bikini for you!";
+        exit;
+    }
+    else if(input1 == 2){
+        std::cout << "Great, Eun-Bi will wear a nice bikini for you!";
+        exit;
+
+    }
+    else{
+        std::cout << "brooo wrong input, try again.";
+
+    }
+    
+    std::cout << "\n--------------------------------" << std::endl;
     return 0;
+    
+
 }
